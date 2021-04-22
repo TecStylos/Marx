8785f1c52f074f026b24c13f58438150f8241366
TecStylos <tecstylos@gmail.com>
2021-04-22 20:38:54 +0200

    Fixed issue in DonHugo to work with new imgui docking branch Added linkage to Xinput.lib

--- DonHugo/src/Application.cpp
+++ DonHugo/src/Application.cpp
@@ -691,8 +691,8 @@ private:
 		ImGuiDockNodeFlags dockspaceFlags = 0;
 
 		ImGuiViewport* viewport = ImGui::GetMainViewport();
-		ImGui::SetNextWindowPos(viewport->GetWorkPos());
-		ImGui::SetNextWindowSize(viewport->GetWorkSize());
+		ImGui::SetNextWindowPos(viewport->WorkPos);
+		ImGui::SetNextWindowSize(viewport->WorkSize);
 		ImGui::SetNextWindowViewport(viewport->ID);
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0.0f, 0.0f });
 		ImGui::Begin("Dockspace", nullptr, windowFlags);
