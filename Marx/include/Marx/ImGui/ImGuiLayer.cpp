3f66c78927715c8555a9d6ea24e6ceeb7717e0ca
TecStylos <tecstylos@gmail.com>
2021-04-23 13:26:51 +0200

    Added submodules glfw and glm

--- Marx/include/Marx/ImGui/ImGuiLayer.cpp
+++ Marx/include/Marx/ImGui/ImGuiLayer.cpp
@@ -13,6 +13,9 @@
 #elif defined MX_PLATFORM_UNIX
 	#include "backends/imgui_impl_glfw.h"
 	#include "backends/imgui_impl_opengl3.h"
+
+	#include <GLFW/glfw3.h>
+	#include <glad/glad.h>
 #endif
 
 namespace Marx
